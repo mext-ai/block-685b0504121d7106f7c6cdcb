b141746d1e5b49a5a68fa849ed65df47ae3927f4
Fabien Sabatié <fabien.sabatie@live.fr>
2025-06-24 22:20:43 +0200

    fix

--- src/index copy.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-// This is the entry point for development/testing
-// The actual federation module exports App directly
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(<App />); 
