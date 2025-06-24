7e938e741455a8c3b7102e3069fda53c67a073c0
Fabien Sabatié <fabien.sabatie@live.fr>
2025-06-24 22:31:52 +0200

    Update block.tsx

--- src/block.tsx
+++ src/block.tsx
@@ -11,15 +11,11 @@ const Block: React.FC<BlockProps> = () => {
         type: 'BLOCK_COMPLETION', 
         blockId: '685b0504121d7106f7c6cdcb', 
         completed: true,
-        score: score,
-        maxScore: 100
       }, '*');
       window.parent?.postMessage({ 
         type: 'BLOCK_COMPLETION', 
         blockId: '685b0504121d7106f7c6cdcb', 
         completed: true,
-        score: score,
-        maxScore: 100
       }, '*');
   }, []);
   return (
